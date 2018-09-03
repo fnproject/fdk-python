548dbe06cac0cb59e476ef5914858c2f51d0b7f4
Denis Makogon <lildee1991@gmail.com>
2018-09-03 14:45:37 -0700

    Init images
     - make the boilerplate to be a problem of an FDK but not a CLI
     - 2 init images for both Python 3.6 and 3.7
     - implemeted with respect to Fn dev docs

---
+++ images/init/3.7/func.py
@@ -0,0 +1,14 @@
+import fdk
+import ujson
+
+
+def handler(ctx, data=None, loop=None):
+    name = "World"
+    if data and len(data) > 0:
+        body = ujson.loads(data)
+        name = body.get("name")
+    return {"message": "Hello {0}".format(name)}
+
+
+if __name__ == "__main__":
+    fdk.handle(handler)
