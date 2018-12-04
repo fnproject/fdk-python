e21c0895b306102044318b263bc68b2bd7f7c416
Reed Allman <reed@rdallman.com>
2018-12-03 18:45:49 -0800

    WIP: stdlib fdk experiment
    note: this is totally a toy and doesn't implement an fdk interface, this just
    sets out to talk the fn http unix socket protocol to see if it's possible to
    do without a bunch of libraries. if this is possible, it's not much of a
    stretch to add an interface around all this, I'm just not good at python and
    will let somebody else do that probably.
    
    this uses stdlib methods to make a unix socket server and serve http requests
    this took a few hours to throw together after reading the stdlib docs and
    searching for examples. got it returning a 200 anyway, so I mean think this
    thing is okay for our purposes honestly? we don't need a robust http server
    for this thing at all.

--- samples/echo/sync/func.py
+++ samples/echo/sync/func.py
@@ -1,25 +1,36 @@
-# All Rights Reserved.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+import socketserver
+import os
+from http.server import BaseHTTPRequestHandler
 
-import fdk
-import json
+class UnixHTTPServer(socketserver.UnixStreamServer):
+    def get_request(self):
+        request, client_address = self.socket.accept()
+        # BaseHTTPRequestHandler expects a tuple with the client address at index
+        # 0, so we fake one
+        if len(client_address) == 0:
+          client_address = (self.server_address,)
+        return (request, client_address)
 
+class myHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        # XXX(reed)
+        # body = self.rfile.read(int(self.headers.getheader('Content-Length')))
 
-def handler(ctx, data=None):
-    body = json.loads(data) if len(data) > 0 else {"name": "World"}
-    return "Hello {0}".format(body.get("name"))
-
+        self.send_response(200)
+        self.send_header('Content-Type','text/ascii')
+        self.end_headers()
+        self.wfile.write("yo yo yo!")
+        return
 
 if __name__ == "__main__":
-    fdk.handle(handler)
+    socket_path = '/tmp/iofs/lsnr.sock'
+    phony_socket_path = '/tmp/iofs/phony.sock'
+    try:
+        with UnixHTTPServer(phony_socket_path, myHandler, False) as server:
+            server.server_bind()
+            os.chmod(phony_socket_path, 0o666)
+            os.symlink(os.path.basename(phony_socket_path), socket_path)
+            server.server_activate()
+            server.serve_forever()
+    except Exception as ex:
+        print(ex)
