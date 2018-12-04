03e2d75ddcaf06755547e933dffc7362c0785f32
Reed Allman <reed@rdallman.com>
2018-12-04 12:02:31 -0800

    asyncio working with h11
    this uses asyncio unix server from stdlib instead of the sync unix server from
    stdlib, and then uses h11 to parse the http parts out. this probably doesn't
    handle timeouts which we may want to do at some level (we get a deadline from
    the fdk protocol) and flow control, which we may not need to handle given what
    we're doing here. anyway, this thing works in the most toy-ish sense, and had
    to upgrade to 3.7 to do asyncio.run - I'm not good at this, that's easy to
    fix I would assume (as we should support 3.6? intention wasn't to change
    python version support, at least, to be clear).
    
    notably, this dependency is really small, and doesn't add to image build times
    or size very much for our base image (like on master). h11 also has no other
    dependencies itself. on master, on my laptop, the build loop was longer and
    images were much larger on master. also, on master, on my laptop i'd get the 5
    second startup timeout. with this function I do not get the 5 second startup
    timeout when using h11 and the asyncio socket. at least, this is looking like
    a pretty plausible option; my laptop is a relatively similarly constrained
    environment for various reasons (thanks McAfee) so this bodes well. the code
    itself, as you can see, it's about 50 LOC to do this, which is not bad at all
    - the h11 dep itself is about 1k LOC, which is about what we were looking at
    for our own http parser - which is an option, but the latter needs to write a
    ton of tests, which h11 has already done, so...
    
    if this path looks okay (we should discuss and weigh), then maybe or hopefully
    somebody with some real python skills can pick this task up from me to make
    the fdk all nice and actually parse our headers, etc, etc.

--- samples/echo/sync/func.py
+++ samples/echo/sync/func.py
@@ -1,36 +1,55 @@
-import socketserver
+import asyncio
+import h11
 import os
-from http.server import BaseHTTPRequestHandler
 
-class UnixHTTPServer(socketserver.UnixStreamServer):
-    def get_request(self):
-        request, client_address = self.socket.accept()
-        # BaseHTTPRequestHandler expects a tuple with the client address at index
-        # 0, so we fake one
-        if len(client_address) == 0:
-          client_address = (self.server_address,)
-        return (request, client_address)
+class HTTPProtocol(asyncio.Protocol):
 
-class myHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        # XXX(reed)
-        # body = self.rfile.read(int(self.headers.getheader('Content-Length')))
+    def __init__(self):
+        self.connection = h11.Connection(h11.SERVER)
 
-        self.send_response(200)
-        self.send_header('Content-Type','text/ascii')
-        self.end_headers()
-        self.wfile.write("yo yo yo!")
-        return
+    def connection_made(self, transport):
+        self.transport = transport
 
-if __name__ == "__main__":
+    def data_received(self, data):
+        self.connection.receive_data(data)
+        while True:
+            event = self.connection.next_event()
+            if isinstance(event, h11.Request):
+                self.send_response(event)
+            elif (
+                isinstance(event, h11.ConnectionClosed)
+                or event is h11.NEED_DATA or event is h11.PAUSED
+            ):
+                break
+        if self.connection.our_state is h11.MUST_CLOSE:
+            self.transport.close()
+
+    def send_response(self, event):
+        body = b"%s %s" % (event.method.upper(), event.target)
+        headers = [
+            ('content-type', 'text/plain'),
+            ('content-length', str(len(body))),
+        ]
+        response = h11.Response(status_code=200, headers=headers)
+        self.send(response)
+        self.send(h11.Data(data=body))
+        self.send(h11.EndOfMessage())
+
+    def send(self, event):
+        data = self.connection.send(event)
+        self.transport.write(data)
+
+async def main():
     socket_path = '/tmp/iofs/lsnr.sock'
     phony_socket_path = '/tmp/iofs/phony.sock'
-    try:
-        with UnixHTTPServer(phony_socket_path, myHandler, False) as server:
-            server.server_bind()
-            os.chmod(phony_socket_path, 0o666)
-            os.symlink(os.path.basename(phony_socket_path), socket_path)
-            server.server_activate()
-            server.serve_forever()
-    except Exception as ex:
-        print(ex)
+
+    loop = asyncio.get_running_loop()
+    server = await loop.create_unix_server(HTTPProtocol, path=phony_socket_path)
+
+    # fix perms on sock and symlink to real sock
+    os.chmod(phony_socket_path, 0o666)
+    os.symlink(os.path.basename(phony_socket_path), socket_path)
+
+    await server.serve_forever()
+
+asyncio.run(main())
