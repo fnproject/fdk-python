9efcb2f83b883611e845dcf0621b5264c0d6bae2
Denis Makogon <denys.makogon@oracle.com>
2019-04-18 23:57:33 +0300

    hide snyk token

--- test-images.sh
+++ test-images.sh
@@ -4,5 +4,5 @@ set -xe
 
 pyversion=${1:-"3.7.1"}
 
-SNYK_TOKEN=${SNYK_TOKEN} snyk test --docker fnproject/python:${pyversion}-dev --file=images/build-stage/${pyversion}/Dockerfile --json | docker run --rm -i denismakogon/snyk-filter:0.0.6
-SNYK_TOKEN=${SNYK_TOKEN} snyk test --docker fnproject/python:${pyversion} --file=images/runtime/${pyversion}/Dockerfile --json | docker run --rm -i denismakogon/snyk-filter:0.0.6
+snyk test --docker fnproject/python:${pyversion}-dev --file=images/build-stage/${pyversion}/Dockerfile --json | docker run --rm -i denismakogon/snyk-filter:0.0.6
+snyk test --docker fnproject/python:${pyversion} --file=images/runtime/${pyversion}/Dockerfile --json | docker run --rm -i denismakogon/snyk-filter:0.0.6
