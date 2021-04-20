ab820dcd1ee11bca3b1fb7daec5f9d0ba2f263a9
Gaurav Saini <gaurav.s.saini@oracle.com>
2021-04-20 10:58:44 +0530

    Added license files and other files to build wheel

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@
 
 import setuptools
 
-
-setuptools.setup(
-    setup_requires=['pbr>=2.0.0'],
-    pbr=True)
+setuptools.setup(setup_requires=['pbr>=2.0.0'],
+                 pbr=True,
+                 data_files=['ChangeLog'])
