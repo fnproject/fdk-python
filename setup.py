e4aacc17b6f2285330f2b9c361ceb6a6fb0ea1b1
CI <ci@fnproject.com>
2021-07-28 05:01:14 +0000

    Releasing version 0.1.34

--- setup.py
+++ setup.py
@@ -16,4 +16,8 @@
 
 import setuptools
 
-setuptools.setup(setup_requires=['pbr>=2.0.0'], pbr=True)
+setuptools.setup(setup_requires=['pbr>=2.0.0'],
+                 pbr=True,
+                 packages=setuptools.find_packages(
+                     exclude=["internal", "internal.*"])
+                 )
