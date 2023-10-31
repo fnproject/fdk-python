6581a085b212c59942d7630fffaed01307a9faab
jayarora <jaytee.arora@oracle.com>
2023-10-31 17:33:44 +0000

    pypi python 3.11

--- internal/release/release_dist_pkg.sh
+++ internal/release/release_dist_pkg.sh
@@ -16,25 +16,15 @@
 #
 set -ex
 
-if [[ "$BUILD_VERSION" == *"SNAPSHOT"* ]]; then
-  echo "Releases will not be performed for snapshot versions, aborting."
-  exit 0
-fi
+echo "Deploy dist source and wheel files to Prod PyPi"
+python3.11 -m venv .release_dist_pkg_venv && source .release_dist_pkg_venv/bin/activate
+echo "Python Version"
+python --version
+pip3 install --upgrade pip
+pip3 install urllib3==1.26.15
+pip3 install twine
+echo "Release version ${BUILD_VERSION}"
+twine upload -u "${FN_PYPI_USER}" -p "${FN_PYPI_PSWD}" dist/fdk-${BUILD_VERSION}*
+deactivate
+rm -rf .release_dist_pkg_venv
 
-RUN_TYPE=${RUN_TYPE:-dry-run}
-export RUN_TYPE
-
-# Release the dist source and wheel files to Prod PyPi
-if [ "${RUN_TYPE}" = "release" ]; then
-  echo "Deploy dist source and wheel files to Prod PyPi"
-  python3.11 -m venv .release_dist_pkg_venv && source .release_dist_pkg_venv/bin/activate
-  echo "Python Version"
-  python --version
-  pip3 install --upgrade pip
-  pip3 install urllib3==1.26.15
-  pip3 install twine
-  echo "Release version ${BUILD_VERSION}"
-  twine upload -u "${FN_PYPI_USER}" -p "${FN_PYPI_PSWD}" dist/fdk-${BUILD_VERSION}*
-  deactivate
-  rm -rf .release_dist_pkg_venv
-fi
--- internal/release/release_dist_pkg_helper.sh
+++ internal/release/release_dist_pkg_helper.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+
 set -ex
 
-BUILD_VERSION=${BUILD_VERSION:-1.0.0-SNAPSHOT}
-LOCAL=${LOCAL:-true}
+if [[ "$BUILD_VERSION" == *"SNAPSHOT"* ]]; then
+  echo "Releases will not be performed for snapshot versions, aborting."
+  exit 0
+fi
 
-export BUILD_VERSION
-export LOCAL
+RUN_TYPE=${RUN_TYPE:-dry-run}
+export RUN_TYPE
 
-(
-  # Release the dist source and wheel files to Prod PyPi
-  docker build -t fdk_python_build_image -f ./internal/docker-files/Dockerfile_dist_pkg .
-  docker run --rm -v $PWD:/build -w /build --env BUILD_VERSION=${BUILD_VERSION} fdk_python_build_image ./internal/release/release_dist_pkg.sh
-)
+# Release the dist source and wheel files to Prod PyPi
+if [ "${RUN_TYPE}" = "release" ]; then
+      docker build -t fdk_python_build_image -f ./internal/docker-files/Dockerfile_dist_pkg .
+      docker run --rm -v $PWD:/build -w /build --env BUILD_VERSION=${BUILD_VERSION} --env FN_PYPI_USER=${FN_PYPI_USER} --env FN_PYPI_PSWD=${FN_PYPI_PSWD} fdk_python_build_image ./internal/release/release_dist_pkg.sh
+fi
+
